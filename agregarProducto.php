7b3edeaae6bcf2292d6ff62b4e1837de7d04ac03
josueJimenez97 <josuejnumss97@gmail.com>
2020-07-12 21:17:17 -0400

    todo posi

--- agregarProducto.php
+++ agregarProducto.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: *'); 
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     require_once './conexion.php';
-    require_once './clases/Producto.php';
+    require_once './clases/Producto';
     $con=Conexion::obtenerConexion(); //ABRIMOS CONEXION A POSTGRES CONFIGUAR config.php CON LO DATOS DE SU POSTGRES
     $json = file_get_contents('php://input');
     $prod=json_decode($json);
