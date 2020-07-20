b71121d947dd5e9673fbaad64d8942e21b854d35
mruniverse8 <ronaldofj83@gmail.com>
2020-07-20 17:08:15 -0400

    agregandor visita (sin testear)

--- obtenerProductosTipo.php
+++ obtenerProductosTipo.php
@@ -4,31 +4,27 @@
     require_once './conexion.php';
     require_once './clases/producto.php';
     $con=Conexion::obtenerConexion(); //ABRIMOS CONEXION A POSTGRES CONFIGUAR config.php CON LO DATOS DE SU POSTGRES
-    $json = file_get_contents('php://input');
+    $idtipo = $_GET['idTipo'];
     
-    $prod=json_decode($json);
-    
-    $consulta= $con->prepare("SELECT*
+    /*$consulta= $con->prepare("SELECT*
     FROM(
       SELECT \"nombre_producto\",\"imagen\",\"cantidad\",\"Tipo_Producto_idtipo\"
         FROM \"Item\"  as item, \"Producto\" as prod
       WHERE item.\"Producto_id_producto\"= prod.\"id_producto\" AND
          item.\"Producto_Tipo_Producto_idtipo\"=prod.\"Tipo_Producto_idtipo\"
       ) as dos, \"Tipo_Producto\" as tipo
-    WHERE dos.\"Tipo_Producto_idtipo\"=tipo.\"idtipo\" AND tipo.\"nombretipo\"='Obra Gruesa'");
-    $consulta->execute();
-    //$consulta->execute([$prod->categoria]);
-    /*
-    if ($consulta->fetch(PDO::FETCH_OBJ))
-    {
-        print("yes");
-    }
+    WHERE dos.\"Tipo_Producto_idtipo\"=tipo.\"idtipo\" AND tipo.\"idtipo\"=?");
+      //$consulta->execute([$prod->categoria]);
     */
+    $consulta= $con->prepare("SELECT *
+                              FROM getprodtipo( ? )");
+    $consulta->execute([$idtipo]);
+    
     $resultado=$consulta->fetchAll();
     
     $listaProductos=array();
     foreach($resultado as $fila){
-        $producto=new Producto($fila[0],$fila[5],$fila[1],$fila[2]);
+        $producto=new Producto($fila[0],$fila[1],$fila[2],$fila[3]);
         array_push($listaProductos,$producto);
     }
     header('Content-Type: application/json');
