ff188816511d820a97a5b6becb17918c264f0690
mruniverse8 <ronaldofj83@gmail.com>
2020-07-24 18:31:59 -0400

    agregando items+++

--- agregarItem.php
+++ agregarItem.php
@@ -6,25 +6,43 @@
     
     $con=Conexion::obtenerConexion(); //ABRIMOS CONEXION A POSTGRES CONFIGUAR config.php CON LO DATOS DE SU POSTGRES
     $miRespuesta="correcto";
+    $json = file_get_contents('php://input');
+    $ITEM=json_decode($json);
 
-    $categoria= $_GET['idtipo'];
-    $idproducto= $_GET['idproducto'];
-    $fabrica= $_GET['fabrica'];
-    $cantidad= $_GET['cantidad'];
-    $precio= $_GET['precio'];
+    $categoria= $ITEM->idCategoria;
+    $idproducto= $ITEM->idProducto;
+    $fabrica= $ITEM->fabrica;
+    $cantidad= $ITEM->cantidad;
+    $precio= $ITEM->precio;
 
     $consulta= $con->prepare("select * from getidfabrica(?)");
     $consulta->execute($fabrica);
     $idfabrica=$consulta->fetch(PDO::FETCH_OBJ);// testear esto
-    if (!$idfabrica || !$categoria || !$idproducto || !$fabrica || !$cantidad)
-        $miRespuesta="incorrecto";
-
-    $consulta= $con->prepare("select * from fadd_item(?,?,?,?)");
-    $consulta->execute($categoria,$idproducto,$idfabrica,$cantidad,$precio);
-    $idfabrica=$consulta->fetch(PDO::FETCH_OBJ);
-
+    
+    $idfabrica=$idfabrica->getidfabrica;
+    
     if (!$idfabrica)
-        $miRespuesta="incorrecto";
+    {
+        $consulta=$con->prepare("insert into \"Fabrica\"(nombrefabrica) values (?);");
+        $consulta->execute($fabrica);
+
+
+        $consulta= $con->prepare("select * from getidfabrica(?)");
+        $consulta->execute($fabrica);
+
+        $idfabrica=$consulta->fetch(PDO::FETCH_OBJ);// testear esto
+        $idfabrica=$idfabrica->getidfabrica;
+    }
+    if (!$idfabrica || !$categoria || !$idproducto || !$fabrica || !$cantidad){
+        $miRespuesta="incorrecto2";
+    }
+    
+    $consulta2= $con->prepare("select * from fadd_item(?,?,?,?,?)");
+    $consulta2->execute([$categoria,$idproducto,$idfabrica,$cantidad,$precio]);
+    $resultado=$consulta2->fetch(PDO::FETCH_OBJ);
+
+    if (!$resultado)
+        $miRespuesta="incorrecto3";
     
     
     header('Content-Type: application/json');
